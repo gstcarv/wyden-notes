f651b9bffe967a9c04b7ae06ae8391c8a6ef2a94
Gustavo Carvalho <gstcarv@gmail.com>
2022-03-15 23:04:04 -0300

    Webpack and Typescript configuration

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,13 +1,31 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-    mode: 'development',
+    mode: "development",
+    entry: "./src/app.ts",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
     devServer: {
-        port: 8085
+        port: 8085,
     },
     plugins: [
         new HtmlWebpackPlugin({
-          template: './index.html'
-        })
-    ]
-}
+            template: "./public/index.html",
+        }),
+    ],
+};
